0eef0f9bd907afcc7c257804d87c015fd5089a77
healeruu <161153756+healeruu@users.noreply.github.com>
2024-03-14 19:11:39 +0800

    pull commit

--- src/main/java/hi.java
+++ src/main/java/hi.java
@@ -5,6 +5,7 @@ public class hi {
         System.out.println("hi git3!");
         System.out.println("hi git4!");
         System.out.println("push test!");
+        System.out.println("pull test!");
 
     }
 }
