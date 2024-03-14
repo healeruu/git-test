384aafadfe89b02cb6e47a173fe895dc1664b9eb
wy <2019227438@qq.com>
2024-03-14 18:32:39 +0800

    third commit

--- src/main/java/hi.java
+++ src/main/java/hi.java
@@ -2,6 +2,7 @@ public class hi {
     public static void main(String[] args){
         System.out.println("hi git!");
         System.out.println("hi git2!");
+        System.out.println("hi git3!");
 
     }
 }
